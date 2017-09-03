7fd908fb2aadcb58918177d331439334702f645e
vlad-ki <inweb24.vk@gmail.com>
2017-09-03 12:08:27 +0300

    corect creating of main table

--- app/static/main.js
+++ app/static/main.js
@@ -4,10 +4,11 @@
 	function createHeaderField(fieldsNames){
 
 		var trItem = document.createElement('tr')
+		fieldsNames.unshift('e')
 
 		for (name in fieldsNames) {
 			var thItem = document.createElement('th')
-			thItem.innerText = name
+			thItem.innerText = fieldsNames[name]
 			trItem.appendChild(thItem)
 		}
 		table.appendChild(trItem)
@@ -15,13 +16,22 @@
 
 
 	function createTableFields(data, fieldsNames){
-		var trItem = document.createElement('tr')
-		for (var i in fieldsNames) {
-			var tdItem = document.createElement('td')
-			tdItem.innerText = data[i]
-			trItem.appendChild(tdItem)
+		for (var line in data) {
+			var aItem = document.createElement('a')
+			var trItem = document.createElement('tr')
+
+			aItem.setAttribute('href', editUrl + data[line].id )
+			aItem.innerText = 'e'
+			trItem.appendChild(aItem)
+
+			for (var fieldName in fieldsNames) {
+				var tdItem = document.createElement('td')
+
+				tdItem.innerText = data[line][fieldsNames[fieldName]]
+				trItem.appendChild(tdItem)
+			}
+			table.appendChild(trItem)
 		}
-		table.appendChild(trItem)
 	}
 
 
@@ -37,12 +47,17 @@
 		
 	}
 
-
+	// fieldsNames is an Array witch containst fealds names for main table
+	/*
+	 data is an array of objects witch contaist maps like {'id': 'idValue', fiel_name1': 'value1', fiel_nameN': 'valueN', ...}
+	 the id is a unique identifer of the note in DB
+	*/
 	var fieldsNames = getData('/table/main/fieldsnames')
 	var data = getData('/table/main/data')
-
+	var editUrl = '/crud/edit/'
 	var table = document.getElementsByClassName('table__main')[0]
-	createHeaderField(fieldsNames)
-	createTableFields(data, fieldsNames)
+	
+	createHeaderField()
+	createTableFields()
 
 }());
